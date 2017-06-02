466101082bf0b4268e3355dd9c323d0c85293c44
Tricia Ball <trish@ted.com>
2017-06-02 09:46:52 -0500

    Check if branch suggestion button already exists

--- insert_branch_names.js
+++ insert_branch_names.js
@@ -14,13 +14,15 @@ function displayBranchNameSuggestions() {
     id = $(this).find('input.id')[0].value;
     ticket_title = $(this).find('[name="story[name]"]')[0].innerHTML;
     slug = slugify(id + "-" + ticket_title);
-    $(this).find('section.controls').append(
-      '<div><button type="button" title="Copy branch name suggestion to clipboard" data-clipboard-text="' +
-        slug +
-        '" class="clipboard_button hoverable left_endcap" tabindex="-1">' +
-        slug +
-        '</button></div>'
-    );
+    if ($(this).find('section.controls .branch_suggestion').length === 0) {
+      $(this).find('section.controls').append(
+        '<div><button type="button" title="Copy branch name suggestion to clipboard" data-clipboard-text="' +
+          slug +
+          '" class="branch_suggestion clipboard_button hoverable left_endcap" tabindex="-1">' +
+          slug +
+          '</button></div>'
+      );
+    }
   });
 }
 
